d932cdfb8dc2bc1e5e82767e4bfd28bbb899e0a3
zfelice <zachfelice@aol.com>
2019-12-23 22:43:35 -0500

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace imageCollageTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter =
+                "Image Files (*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            dialog.Multiselect = true;
+            Random rand1 = new Random();
+            Random rand2 = new Random();
+
+            if (dialog.ShowDialog() == true)
+            {
+                string[] images = dialog.FileNames;
+                foreach(string image in images)
+                {
+                    int rand1_1 = rand1.Next(0, 400);
+                    int rand2_2 = rand2.Next(0, 400);
+                    var bitmap = new BitmapImage(new Uri(image));
+                    var img = new Image { Source = bitmap };
+                    Canvas.SetLeft(img, rand1_1);
+                    Canvas.SetTop(img, rand2_2);
+                    canvas.Children.Add(img);
+                }
+            }
+        }
+
+        private Image draggedImage;
+        private Point mousePosition;
+
+        private void CanvasMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var image = e.Source as Image;
+
+            if (image != null && canvas.CaptureMouse())
+            {
+                mousePosition = e.GetPosition(canvas);
+                draggedImage = image;
+                Panel.SetZIndex(draggedImage, 1); // in case of multiple images
+            }
+        }
+
+        private void CanvasMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (draggedImage != null)
+            {
+                canvas.ReleaseMouseCapture();
+                Panel.SetZIndex(draggedImage, 0);
+                draggedImage = null;
+            }
+        }
+
+        private void CanvasMouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggedImage != null)
+            {
+                var position = e.GetPosition(canvas);
+                var offset = position - mousePosition;
+                mousePosition = position;
+                Canvas.SetLeft(draggedImage, Canvas.GetLeft(draggedImage) + offset.X);
+                Canvas.SetTop(draggedImage, Canvas.GetTop(draggedImage) + offset.Y);
+            }
+        }
+    }
+}
